f48ee8231a1987871f82ace03777759d90c89ae0
Robin Mueller <robin.mueller.m@gmail.com>
2024-07-04 18:55:30 +0200

    another link correction

--- va108xx-hal/src/timer.rs
+++ va108xx-hal/src/timer.rs
@@ -3,7 +3,7 @@
 //! ## Examples
 //!
 //! - [MS and second tick implementation](https://egit.irs.uni-stuttgart.de/rust/va108xx-rs/src/branch/main/examples/simple/examples/timer-ticks.rs)
-//! - [Cascade feature example](https://egit.irs.uni-stuttgart.de/rust/va108xx-hal/src/branch/main/examples/cascade.rs)
+//! - [Cascade feature example](https://egit.irs.uni-stuttgart.de/rust/va108xx-rs/src/branch/main/examples/simple/examples/cascade.rs)
 pub use crate::IrqCfg;
 use crate::{
     clock::{enable_peripheral_clock, PeripheralClocks},
