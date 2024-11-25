c6314f48d7b605b2a94c85be93f935c8fbb76dd0
Robin Mueller <robin.mueller.m@gmail.com>
2024-11-25 16:10:54 +0100

    update probe-rs files

--- jlink-gdb.sh
+++ jlink-gdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-JLinkGDBServer -select USB -device Cortex-M0 -endian little -if JTAG-speed auto \
+JLinkGDBServer -select USB -device Cortex-M0 -endian little -if JTAG -speed auto \
   -LocalhostOnly
